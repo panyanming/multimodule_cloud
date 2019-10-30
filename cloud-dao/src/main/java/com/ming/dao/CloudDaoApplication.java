fcfd55aa22556de49ad93e843ba323e38c817caa
panyanming <panyanming@autohome.com.cn>
2019-10-30 14:43:15 +0800

    集成druid

---
+++ cloud-dao/src/main/java/com/ming/dao/CloudDaoApplication.java
@@ -0,0 +1,13 @@
+package com.ming.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDaoApplication.class, args);
+	}
+
+}
